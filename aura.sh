3aba004c493a41894d969bd87c8b38d77cfd5826
Pedro Mendes <pedro.mendes.26@gmail.com>
2020-07-23 01:17:06 +0100

    Add edit mode, passing trailling arguments to makepkg, double quotes (#1)
    * Add edit mode, passing trailling arguments to makepkg, double quotes
    
    * Remove tabs

--- aura.sh
+++ aura.sh
@@ -1,67 +1,69 @@
 #!/bin/bash
+#shellcheck disable=2155
 
 shopt -s extglob
 
 aur() {
-    while [ $# -gt 0 ]; do 
-	    pushd /tmp >> /dev/null
-	    git clone https://aur.archlinux.org/$1
-	    cd $1
-	    makepkg -si
-	    cd ..
-	    rm -rf $1
-	    popd >> /dev/null
+    while [ $# -gt 0 ]; do
+        pushd /tmp >> /dev/null || return
+        git clone https://aur.archlinux.org/"$1"
+        cd "$1" || return
+        [ "$edit" ] && ${EDITOR:-vi} PKGBUILD
+        makepkg -si --clean "${@:2}"
+        cd ..
+        rm -rf "$1"
+        popd >> /dev/null || return
         shift
     done
 }
 
 aurs() {
-	curl -s "https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg=$1" | jq '.results[] | "\(.Name) -> \(.Description)"'
+    curl -s "https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg=$1" |
+        jq '.results[] | "\(.Name) -> \(.Description)"'
 }
 
 auru() {
     local PACKAGES=$(pacman -Q)
     local TOTAL=$(pacman -Q | wc -l)
 
-    for i in $(seq 0 200 $TOTAL); do
-        local UPDATES+=$(echo $PACKAGES \
+    for i in $(seq 0 200 "$TOTAL"); do
+        local UPDATES+=$(echo "$PACKAGES" \
             | tr ' ' '\n' \
             | tail -n $((TOTAL - i)) \
             | head -n 200 \
             | cut -d" " -f1 \
             | tr '\n' ' ' \
-            | curl -s $(echo "https://aur.archlinux.org/rpc/?v=5&type=info"$(sed -r 's/(\S*)\s/\&arg[]=\1/g')));
+            | curl -s "https://aur.archlinux.org/rpc/?v=5&type=info$(sed -r 's/(\S*)\s/\&arg[]=\1/g')");
     done;
 
-    local AURPKG=$(echo $UPDATES \
+    local AURPKG=$(echo "$UPDATES" \
                     | jq -M '.results[] | "\(.Name)"' \
                     | tr -d '"')
 
     for f in $AURPKG; do
-        local PKGVER+=$(echo "$(pacman -Qs "^$f\$" | cut -d" " -f2) ");
+        local PKGVER+=("$(pacman -Qs "^$f\$" | cut -d" " -f2) ");
     done;
 
-    local i=1 
-    local UPS=$(echo $UPDATES \
+    local i=1
+    local UPS=$(echo "$UPDATES" \
         | jq '.results[] | "\(.Name)>\(.Version)"' \
         | tr -d '"')
-    
-    for ver in $PKGVER; do
-        local FINAL+=$(echo "$(echo $UPS| cut -d" " -f$i)>$ver ")
+
+    for ver in "${PKGVER[@]}"; do
+        local FINAL+=("$(echo "$UPS" | cut -d" " -f$i)>$ver ")
         i=$((i+1));
     done;
 
-    echo $FINAL \
+    echo "${FINAL[@]}" \
        | tr ' ' '\n' \
        | sed -E 's/([^>]+)>([^>]+)>([^>]+)/\1>\3>\2/g' \
        | column -ts'>' -N PKG,INSTALED,REMOTE
 
-    echo Wanna Update?
     local conf
-    read conf
+    read -r -p 'Wanna Update? ' conf
     case $conf in
-        y|yes)
-            aur `echo $FINAL | sed -E 's/([^>]+)>([^>]+)>([^ ]+)/\1/g'`
+        y|yes|Y|Yes)
+            aur "$(echo "${FINAL[@]}" | sed -E 's/([^>]+)>([^>]+)>([^ ]+)/\1/g')"
             ;;
         *)
             echo Bye
@@ -69,18 +71,26 @@ auru() {
     esac
 }
 
-case $1 in
-    -R*)
-        sudo pacman -Rsn $2
-        ;;
-    -S)
-        shift
-        aur "$@"
-        ;;
-    -Ss)
-        aurs $2
-        ;;
-    -S+(y)u )
-        auru
-        ;;
-esac
+main() {
+    case $1 in
+        -e)
+            edit=1
+            main "${@:2}"
+            ;;
+        -R*)
+            sudo pacman -Rsn "$2"
+            ;;
+        -S)
+            shift
+            aur "$@"
+            ;;
+        -Ss)
+            aurs "$2"
+            ;;
+        -S+(y)u )
+            auru
+            ;;
+    esac
+}
+
+main "$@"
